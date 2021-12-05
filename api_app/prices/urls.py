80a00abe072ad38a29bcdefa255ec64cf6280b8d
Cisco <82993356+francescokm@users.noreply.github.com>
2021-12-05 22:29:51 +0700

    urls

--- api_app/prices/urls.py
+++ api_app/prices/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api_app.prices.views import PriceServiceView, DetailPriceServView
 
 urlpatterns = [
-    path("service/", PriceServiceView.as_view(), name="priceserv"),
-    path("service/<int:pk>/", DetailPriceServView.as_view(), name="det_priceserv"),
+    path("price/", PriceServiceView.as_view(), name="priceserv"),
+    path("price/<int:pk>/", DetailPriceServView.as_view(), name="det_priceserv"),
 ]
