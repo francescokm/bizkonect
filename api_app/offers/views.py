e2e3d4830d349b5b5ed2ecdc95b354d57fae9fb5
Cisco <82993356+francescokm@users.noreply.github.com>
2021-12-08 13:04:39 +0700

    perbaikan di offers view

--- api_app/offers/views.py
+++ api_app/offers/views.py
@@ -2,19 +2,19 @@ from rest_framework import generics
 from api_app.offers.models import first_offer as M_1, second_offer as M_2
 from api_app.offers.serializers import FirstOfferSerial,SecondOfferSerial
 
-class CreateFirstOfferView(generics.CreateAPIView):
+class CreateFirstOfferView(generics.ListCreateAPIView):
     queryset = M_1.objects.all()
     serializer_class  = FirstOfferSerial
 
-class DetailFirstOfferView(generics.UpdateAPIView):
+class DetailFirstOfferView(generics.RetrieveAPIView):
     queryset = M_1.objects.all()
     serializer_class  = FirstOfferSerial
 
 
-class CreateSecondOfferView(generics.CreateAPIView):
+class CreateSecondOfferView(generics.ListCreateAPIView):
     queryset = M_2.objects.all()
     serializer_class  = SecondOfferSerial
 
-class DetailSecondOfferView(generics.UpdateAPIView):
+class DetailSecondOfferView(generics.RetrieveAPIView):
     queryset = M_2.objects.all()
     serializer_class  = SecondOfferSerial
